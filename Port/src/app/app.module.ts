292b3fda2176eca2bbd310d318b9dfeb59898efb
MelvinMuuo <muuomelvin73@gmail.com>
2019-08-26 02:34:04 +0300

    create portfolio component and work component

---
+++ Port/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WorkComponent } from './work/work.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortfolioComponent,
+    WorkComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
